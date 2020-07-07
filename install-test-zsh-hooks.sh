9bb2addb94122a5b8595f4d65035a77c21bf4ee7
Daniel Muey <dan@cpanel.net>
2020-07-07 10:42:49 -0500

    ZC-7107: make test hook output clearer; make it work w/ yum

--- install-test-zsh-hooks.sh
+++ install-test-zsh-hooks.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 HOOK_DIRS='
+/etc/yum/universal-hooks/posttrans
+/etc/yum/universal-hooks/pkgs/zsh/posttrans
+/etc/yum/universal-hooks/multi_pkgs/posttrans/zs__WILDCARD__
+'
+
+if [ -x "/usr/bin/dnf" ];
+then
+HOOK_DIRS='
 /etc/dnf/universal-hooks/transaction
 /etc/dnf/universal-hooks/pkgs/zsh/transaction
 /etc/dnf/universal-hooks/multi_pkgs/transaction/zs__WILDCARD__
 '
+fi
 
 for d in $HOOK_DIRS; do
     hook_script=$d/test-hook.sh
@@ -13,7 +22,7 @@ for d in $HOOK_DIRS; do
     cat >$hook_script <<"EOH"
 #!/usr/bin/env bash
 
-echo "the $0 script with args: $*"
+echo "Universal Hook Fired: “$0” with args: $*"
 
 pkg_list=${1#--pkg_list=}
 if [ -n "$pkg_list" ]; then
@@ -21,7 +30,8 @@ if [ -n "$pkg_list" ]; then
     cat $pkg_list
 fi
 
-echo done
+echo " … done ($0)"
+echo ""
 EOH
 
     chmod +x $hook_script
