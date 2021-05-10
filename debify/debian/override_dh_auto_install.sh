db0609287a94f31bd592ca188db224a4765d06c1
Daniel Muey <dan@cpanel.net>
2021-05-10 17:35:25 -0500

    correct name for DPkg::Tools::Options

--- debify/debian/override_dh_auto_install.sh
+++ debify/debian/override_dh_auto_install.sh
@@ -5,7 +5,7 @@ source debian/vars.sh
 # We only support the DPkg:: version of these hook points, if that ever changes
 #  this will need updated here and in apt-universal-hooks.pl
 mkdir -p $DEB_INSTALL_ROOT/etc/apt/apt.conf.d
-echo 'DPkg::Tools::Options::/etc/apt/universal-hooks/apt-universal-hook.pl::Version "2";
+echo 'DPkg::Tools::Options::/etc/apt/universal-hooks/apt-universal-hooks.pl::Version "2";
 DPkg::Pre-Invoke {"/etc/apt/universal-hooks/apt-universal-hooks.pl Pre-Invoke || true";}
 DPkg::Pre-Install-Pkgs {"/etc/apt/universal-hooks/apt-universal-hooks.pl Pre-Install-Pkgs || true";};
 DPkg::Post-Invoke {"/etc/apt/universal-hooks/apt-universal-hooks.pl Post-Invoke || true";}' > apt-universal-hooks.conf
